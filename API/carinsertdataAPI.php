d95984027e78c0fa5297542a1df57eca2480d178
ABHI GHOGHARI <143987211+Abhi-GhoGhari@users.noreply.github.com>
2024-11-26 19:41:57 +0530

    Add files via upload

---
+++ API/carinsertdataAPI.php
@@ -0,0 +1,9 @@
+<?php
+
+    include("connection.php");
+
+    header('Content-Type: application/json');
+
+    $name = $_post['name'];
+    $numplate = $_POST['numplate']
+?>
---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbName = "ecommerce"; 
+
+$conn = mysqli_connect($server,$username,$password,$dbName);
+
+
+?>
