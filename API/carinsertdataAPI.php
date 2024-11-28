5f937e188c206cd644fdcc87008fbf9005fe0e30
ABHI GHOGHARI <143987211+Abhi-GhoGhari@users.noreply.github.com>
2024-11-28 18:11:30 +0530

    First table done

--- API/carinsertdataAPI.php
+++ API/carinsertdataAPI.php
@@ -1,9 +1,34 @@
+
 <?php
+include ("../config.php");
+header('Content-Type: application/json');
 
-    include("connection.php");
+$congin = new Config();
+$conn_res = $congin->connect();
 
-    header('Content-Type: application/json');
+$name = $_POST['car'];
+$arr = array();
 
-    $name = $_post['name'];
-    $numplate = $_POST['numplate']
-?>
+if ($name != null) {
+    if ($conn_res) {
+        $sql = "INSERT INTO `firstflor`(`car`) VALUES ('$name')";
+        $response = mysqli_query($conn_res, $sql);
+        if ($response) {
+            $arr = array("status_code" => "200", "message" => "Data Update Successfully", "is_success" => "true");
+            echo json_encode($arr, JSON_PRETTY_PRINT);
+            http_response_code(200);
+        } else {
+            $arr = array("status_code" => "500", "message" => "Data Update Failed", "is_success" => "false");
+            echo json_encode($arr, JSON_PRETTY_PRINT);
+            http_response_code(500);
+        }
+    } else {
+        $arr = array("status_code" => "500", "message" => "Connection Failed", "is_success" => "false");
+        echo json_encode($arr, JSON_PRETTY_PRINT);
+        http_response_code(500);
+    }
+} else {
+    $arr = array("status_code" => "400", "message" => "Car Name are must be required", "is_success" => "false");
+    echo json_encode($arr, JSON_PRETTY_PRINT);
+    http_response_code(400);
+}
